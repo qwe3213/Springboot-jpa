10938f85079a68be603862917a4569d186841e2c
hayun <hs183000@naver.com>
2023-09-24 23:37:19 +0900

    주문 목록 검색

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.Memberservice;
 import jpabook.jpashop.service.OrderService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,9 +38,16 @@ public class OrderController {
    }
    @PostMapping("/order")
    public String order(@RequestParam("memberId") Long memberId,
-                       @RequestParam("itemId") Long itemid,
+                       @RequestParam("itemId") Long itemId,
                        @RequestParam("count") int count){
-      orderService.order(memberId, itemid, count);
+      orderService.order(memberId, itemId, count);
       return "redirect:/orders";
+   }
+
+   @GetMapping("/orders")
+   public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model){
+      List<Order> orders = orderService.findOrders(orderSearch);
+      model.addAttribute("orders",orders);
+      return "order/orderList";
    }
 }
